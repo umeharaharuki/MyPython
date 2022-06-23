04a3d03853b18e8c82a14e37372d2437111f1459
umeharaharuki <jasper1116lopez@icloud.com>
2022-06-23 16:15:36 +0900

    星形作成

---
+++ turtle3.py
@@ -0,0 +1,9 @@
+from binascii import a2b_hex
+from turtle import*
+shape("turtle")
+col = ["orange","limegreen","gold","plum","tomato"]
+for i in range(5):
+  color(col[i])
+  forward(200)
+  left(144)
+done()
